2d728ccf7e8a92849b11759881d30aa48415c7cf
gyungnam <7646602@gmail.com>
2021-11-19 18:22:25 +0900

    11/19

---
+++ Sorting/정렬 라이브러리(6-7,8,9).py
@@ -0,0 +1,18 @@
+'''sorted
+array = [7,5,9,0,3,1,6,2,4,8]
+result = sorted(array)
+print(result)
+'''
+
+'''sort
+array = [7,5,9,0,3,1,6,2,4,8]
+array.sort()
+print(array)
+'''
+
+#key
+array = [('바나나', 2), ('사과', 5), ('당근', 3)]
+def setting(data):
+    return data[1]
+result = sorted(array, key=setting)
+print(result)
